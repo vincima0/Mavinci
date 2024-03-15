c3126d9d5dd2ebe83cc485313eb080a6d27be2f1
vincima0 <1920340410@qq.com>
2024-03-15 18:07:21 +0800

    modify

--- client-sync/http_client_sync.cpp
+++ client-sync/http_client_sync.cpp
@@ -35,9 +35,9 @@ int main(int argc, char** argv)
     try
     {
        
-        auto const host = "www.njupt.edu.cn";
+        auto const host = "xkb.njupt.edu.cn";
         auto const port = "80";
-        auto const target = "/";
+        auto const target = "/_upload/article/images/14/19/922572fe486f868a4a14739109c0/f1cd1db4-6583-4f11-a7bd-b6d8b07f40b8.jpg";
         int version =  11;
 
         // The io_context is required for all I/O
