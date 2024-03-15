86f0f44c916b960c605221b92df3c4eb66c824ed
vincima0 <1920340410@qq.com>
2024-03-15 17:03:44 +0800

    modify

--- client-sync/http_client_sync.cpp
+++ client-sync/http_client_sync.cpp
@@ -34,20 +34,11 @@ int main(int argc, char** argv)
 {
     try
     {
-        // Check command line arguments.
-        if(argc != 4 && argc != 5)
-        {
-            std::cerr <<
-                "Usage: http-client-sync <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n" <<
-                "Example:\n" <<
-                "    http-client-sync www.example.com 80 /\n" <<
-                "    http-client-sync www.example.com 80 / 1.0\n";
-            return EXIT_FAILURE;
-        }
-        auto const host = argv[1];
-        auto const port = argv[2];
-        auto const target = argv[3];
-        int version = argc == 5 && !std::strcmp("1.0", argv[4]) ? 10 : 11;
+       
+        auto const host = "www.baidu.com";
+        auto const port = "80";
+        auto const target = "/";
+        int version =  11;
 
         // The io_context is required for all I/O
         net::io_context ioc;
