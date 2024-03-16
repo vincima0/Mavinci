23aa62a164457b35ec1a7ddb6a45f717e1149771
vincima0 <1920340410@qq.com>
2024-03-16 16:17:22 +0800

    modify

---
+++ common/temperature_sensor.h
@@ -0,0 +1,21 @@
+class temperature_sensor final
+{
+public:
+    temperature_sensor()
+    {
+
+    }
+
+    ~temperature_sensor()
+    {
+
+    }
+public:
+
+    float get_temperature()
+    {
+
+        return 26;
+    }
+
+};
