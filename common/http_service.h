267a8abafde7b2a030eda72090842fdb23589e7e
vincima0 <1920340410@qq.com>
2024-03-17 20:24:34 +0800

    modify http_service.h

--- common/http_service.h
+++ common/http_service.h
@@ -5,10 +5,10 @@
 #include <boost/beast.hpp>
 
 
-namespace beast = boost::beast;     // from <boost/beast.hpp>
-namespace http = beast::http;       // from <boost/beast/http.hpp>
-namespace net = boost::asio;        // from <boost/asio.hpp>
-using tcp = net::ip::tcp;           // from <boost/asio/ip/tcp.hpp>
+// namespace beast = boost::beast;     // from <boost/beast.hpp>
+// namespace http = beast::http;       // from <boost/beast/http.hpp>
+// namespace net = boost::asio;        // from <boost/asio.hpp>
+// using tcp = net::ip::tcp;           // from <boost/asio/ip/tcp.hpp>
 
 
 class http_service final
@@ -16,13 +16,13 @@ class http_service final
 public:
     static bool send_request(std::string_view host,std::string_view port,
     const boost::beast::http::request<boost::beast::http::string_body>& req,
-    boost::beast::http::response<http::string_body>& res)
+    boost::beast::http::response<boost::beast::http::string_body>& res)
     {
         //the io_context is required for all I/O
         boost::asio::io_context ioc;
         //these objects perform our I/O
-        tcp::resolver resolver(ioc);
-        beast::tcp_stream stream(ioc);
+        net::ip::tcp::resolver resolver(ioc);
+        boost::beast::tcp_stream stream(ioc);
         // look up the domain name
         boost::system::error_code ec;
         auto const results = resolver.resolve(host,port,ec);
@@ -39,14 +39,14 @@ public:
             return false;
         }
         //send the http request to the remote host
-        http::write(stream,req,ec);
+        boost::beast::http::write(stream,req,ec);
         if(ec)
         {
             console::log(std::source_location::current(),host,port,ec);
             return false;
         }
         //this buffer id used for reading and must be persisted
-        beast::flat_buffer buffer;
+        boost::beast::flat_buffer buffer;
         //receive the HTTP response
         http::read(stream,buffer,res,ec);
         if(ec)
@@ -55,8 +55,8 @@ public:
             return false;
         }
 
-        stream.socket().shutdown(tcp::socket::shutdown_both,ec);
-        if(ec && ec !=beast::errc::not_connected)
+        stream.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both,ec);
+        if(ec && ec !=boost::beast::errc::not_connected)
         {
             console::log(std::source_location::current(), host, port, ec);
         }
