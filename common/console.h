eedd76476d2d405b503ab49b313aed6145319369
vincima0 <1920340410@qq.com>
2024-03-17 20:53:15 +0800

    modify console.h

--- common/console.h
+++ common/console.h
@@ -1,7 +1,30 @@
 #pragma once
+
+#include <mutex>
+#include <iostream>
+#include <boost/date_time.hpp>
 class console final
-{
-    public:
+{   
+    inline static std::mutex  s_cout_mutex;//多线程下cout有竞争问题，加一把互斥锁
+    static void Build(std::ostream& log)
+    {
+        log<<std::endl;
+    }
+    template<typename T,typename ...Types>
+    constexpr static void Build(std::ostream& log,const T& firstArg, const Types& ... args)
+    {
+        log<<" "<<firstArg;
+    }
+private:
+    console()= delete;
+    ~console()=delete;
+
+    console(const console&)=delete;
+    console(console&&);
+
+    console& operator =(const console&) =delete;
+    console& operator =(console&&)=delete;
+public:
     static void log(...)
     {
 
