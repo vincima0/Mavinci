96244d994315dc6ef474dc53d9566b785c3a8ab8
vincima0 <1920340410@qq.com>
2024-03-18 21:02:14 +0800

    modify

--- common/http_service.h
+++ common/http_service.h
@@ -15,6 +15,57 @@
 class http_service final
 {
 public:
+    static bool send_temperature(std::string_view host,std::string_view port,float temperature)
+    {
+        boost::json::object data;
+        data["temperature"] = temperature;
+        data["time"] =std::time(nullptr);
+        std::string json_data =boost::json::serialize(data);
+
+        http::request<http::string_body> req{http::verb::post,http_url_router::TEMPERATURE,11};
+        req.set(http::field::host, host);
+        req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+        req.body() = json_data;
+        req.prepare_payload();
+
+
+        http::response<http::string_body> res;
+        if(send_request(host,port,req,res))
+        {
+            console::log(std::source_location::current(),res);
+        } 
+        else
+        {
+            return 0;
+        }
+    }
+
+
+    static bool send_light(std::string_view host,std::string_view port,float light )
+    {
+        boost::json::object data;
+        data["light"] = light;
+        data["time"] =std::time(nullptr);
+        std::string json_data =boost::json::serialize(data);
+
+        http::request<http::string_body> req{http::verb::post,http_url_router::TEMPERATURE,11};
+        req.set(http::field::host, host);
+        req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+        req.body() = json_data;
+        req.prepare_payload();
+
+
+        http::response<http::string_body> res;
+        if(send_request(host,port,req,res))
+        {
+            console::log(std::source_location::current(),res);
+        } 
+        else
+        {
+            return 0;
+        }
+    }
+
     static bool send_request(std::string_view host,std::string_view port,
     const boost::beast::http::request<boost::beast::http::string_body>& req,
     boost::beast::http::response<boost::beast::http::string_body>& res)
